93d4f9e1a843fa66af446fc30ac32018d9563728
kkiriyama <n.kurata1005@gmail.com>
2019-05-02 08:48:49 +0900

    delete firebase test project

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,6 +1,6 @@
 const firebase = require('firebase')
 
-const defaultProductionConfig = {
+const defaultConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   datbaseURL: process.env.FIREBASE_DATABASE_URL,
@@ -9,16 +9,7 @@ const defaultProductionConfig = {
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 
-const defaultTestConfig = {
-  apiKey: process.env.FIREBASE_TEST_API_KEY,
-  authDomain: process.env.FIREBASE_TEST_AUTH_DOMAIN,
-  datbaseURL: process.env.FIREBASE_TEST_DATABASE_URL,
-  projectId: process.env.FIREBASE_TEST_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_TEST_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_TEST_MESSAGING_SENDER_ID
-}
-
-const config = process.NODE_DEPLOY === 'production' ? defaultProductionConfig : defaultTestConfig
+const config = defaultConfig
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
